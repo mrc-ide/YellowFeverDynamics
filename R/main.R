30fff50e5ad65a7f8a373f2d9240671b089159f5
KeithJF82 <keith.fraser@imperial.ac.uk>
2025-06-11 14:55:09 +0000

    Added model version with vaccination track

--- R/main.R
+++ R/main.R
@@ -405,6 +405,84 @@ Model_Run_Delay_Many_Reps <- function(FOI_spillover = c(), R0 = c(), vacc_data =
   return(output_data)
 }
 #-------------------------------------------------------------------------------
+#' @title Model_Run_VTrack
+#'
+#' @description Run SEIR model with separate vaccination track for single region
+#'
+#' @details Accepts epidemiological + population parameters and model settings; runs SEIR model with
+#' separate vaccination track for one region over a specified time period for a number of particles/threads and outputs time-dependent SEIRV
+#' values, infection numbers and/or total force of infection values.
+#'
+#' @param FOI_spillover Vector of values of force of infection due to spillover from sylvatic reservoir
+#'   (size depends on mode_time)
+#' @param R0 Vector of values of basic reproduction number for urban spread of infection (size depends on mode_time)
+#' @param vacc_data Projected vaccination-based immunity (assuming vaccine_efficacy = 1) by age group and year
+#' @param pop_data Population by age group and year
+#' @param years_data Incremental vector of years denoting years for which to save data
+#' @param year0 First year in population/vaccination data
+#' @param vaccine_efficacy Proportional vaccine efficacy
+#' @param time_inc Time increment in days to use in model (should be 1.0, 2.5 or 5.0 days)
+#' @param mode_start Flag indicating how to set initial population immunity level in addition to vaccination \cr
+#'  If mode_start = 0, only vaccinated individuals \cr
+#'  If mode_start = 1, shift some non-vaccinated individuals into recovered to give herd immunity (stratified by age) \cr
+#'  If mode_start = 2, use SEIRV input in list from previous run(s) \cr
+#' @param start_SEIRV SEIRV data from end of a previous run to use as input (if mode_start = 2)
+#'   #TODO - Switch to SEIR multitrack data
+#' @param mode_time Type of time dependence of FOI_spillover and R0 to be used: \cr
+#'  If mode_time = 0, no time variation (constant values)\cr
+#'  If mode_time = 1, FOI/R0 vary annually without seasonality (number of values = number of years to consider) \cr
+#'  If mode_time = 2, FOI/R0 vary with monthly seasonality without inter - annual variation (number of values = 12) \cr
+#'  If mode_time = 3, FOI/R0 vary with daily seasonality without inter - annual variation (number of values = 365/time_inc) \cr
+#'  If mode_time = 4, FOI/R0 vary annually with monthly seasonality (number of values = 12*number of years to consider) \cr
+#'  If mode_time = 5, FOI/R0 vary annually with daily seasonality (number of values = (365/time_inc)*number of years to consider)
+#' @param n_particles number of particles to use
+#' @param n_threads number of threads to use
+#' @param deterministic TRUE/FALSE  -  set model to run in deterministic mode if TRUE
+#' '
+#' @export
+#'
+Model_Run_VTrack <- function(FOI_spillover = 0.0, R0 = 1.0, vacc_data = list(), pop_data = list(), years_data = c(1940:1941),
+                      year0 = 1940, vaccine_efficacy = 1.0, time_inc = 1.0, mode_start = 0,
+                      start_SEIRV = list(), mode_time = 0, n_particles = 1, n_threads = 1, deterministic = FALSE) {
+
+  #TODO Add assert_that functions (NB  -  Some checks carried out in parameter_setup)
+  assert_that(n_particles <= 20, msg = "Number of particles must be 20 or less")
+
+  N_age = length(pop_data[1, ]) #Number of age groups
+  step_begin = ((years_data[1] - year0)*(365/time_inc)) #Step at which data starts being saved for final output
+  step_end = ((max(years_data) + 1 - year0)*(365/time_inc)) - 1 #Step at which to end
+  t_pts_out = step_end - step_begin + 1 #Number of time points in final output data
+
+  pars = parameter_setup(FOI_spillover, R0, vacc_data, pop_data, years_data, year0,
+                         vaccine_efficacy, time_inc, mode_start, start_SEIRV, mode_time)
+  pars2=pars
+  pars2$S_0=pars2$E_0=pars2$I_0=pars2$R_0=array(0,dim=c(N_age,2))
+  pars2$V_0=NULL
+  pars2$S_0[,1]=pars$S_0
+  pars2$S_0[,2]=pars$V_0 #TODO - split vaccinated between S and R
+  pars2$E_0[,1]=pars$E_0
+  pars2$I_0[,1]=pars$I_0
+  pars2$R_0[,1]=pars$R_0
+
+  x <- dust_system_create(SEIRModelVtrack, pars = pars2,
+                          n_particles = n_particles, n_threads = n_threads, time = 0, dt = 1,
+                          deterministic = deterministic, preserve_particle_dimension = TRUE)
+  index = dust_unpack_index(x)
+  dust_system_set_state_initial(x)
+  x_res <- dust_system_simulate(x, times = c(step_begin:step_end))
+
+  dim1 = c(N_age,n_particles, t_pts_out)
+  dim2 = c(N_age, 2, n_particles, t_pts_out)
+  output_data = list(day = x_res[1, 1, ], year = x_res[2, 1, ], FOI_total = x_res[3, , ]/time_inc,
+                     S = array(x_res[index$S, , ], dim2), E = array(x_res[index$E, , ], dim2),
+                     I = array(x_res[index$I, , ], dim2), R = array(x_res[index$R, , ], dim2),
+                     C = array(x_res[index$C, , ], dim1))
+  x_res = NULL
+  gc()
+
+  return(output_data)
+}
+#-------------------------------------------------------------------------------
 #' @title Model_Versions_Run
 #'
 #' @description TBA
--- src/cpp11.cpp
+++ src/cpp11.cpp
@@ -5,6 +5,90 @@
 #include "cpp11/declarations.hpp"
 #include <R_ext/Visibility.h>
 
+// SEIRModelVtrack.cpp
+SEXP dust2_system_SEIRModelVtrack_alloc(cpp11::list r_pars, cpp11::sexp r_time, cpp11::list r_time_control, cpp11::sexp r_n_particles, cpp11::sexp r_n_groups, cpp11::sexp r_seed, cpp11::sexp r_deterministic, cpp11::sexp r_n_threads);
+extern "C" SEXP _YellowFeverDynamics_dust2_system_SEIRModelVtrack_alloc(SEXP r_pars, SEXP r_time, SEXP r_time_control, SEXP r_n_particles, SEXP r_n_groups, SEXP r_seed, SEXP r_deterministic, SEXP r_n_threads) {
+  BEGIN_CPP11
+    return cpp11::as_sexp(dust2_system_SEIRModelVtrack_alloc(cpp11::as_cpp<cpp11::decay_t<cpp11::list>>(r_pars), cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(r_time), cpp11::as_cpp<cpp11::decay_t<cpp11::list>>(r_time_control), cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(r_n_particles), cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(r_n_groups), cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(r_seed), cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(r_deterministic), cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(r_n_threads)));
+  END_CPP11
+}
+// SEIRModelVtrack.cpp
+SEXP dust2_system_SEIRModelVtrack_run_to_time(cpp11::sexp ptr, cpp11::sexp r_time);
+extern "C" SEXP _YellowFeverDynamics_dust2_system_SEIRModelVtrack_run_to_time(SEXP ptr, SEXP r_time) {
+  BEGIN_CPP11
+    return cpp11::as_sexp(dust2_system_SEIRModelVtrack_run_to_time(cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(ptr), cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(r_time)));
+  END_CPP11
+}
+// SEIRModelVtrack.cpp
+SEXP dust2_system_SEIRModelVtrack_state(cpp11::sexp ptr, cpp11::sexp r_index_state, cpp11::sexp r_index_particle, cpp11::sexp r_index_group, bool preserve_particle_dimension, bool preserve_group_dimension);
+extern "C" SEXP _YellowFeverDynamics_dust2_system_SEIRModelVtrack_state(SEXP ptr, SEXP r_index_state, SEXP r_index_particle, SEXP r_index_group, SEXP preserve_particle_dimension, SEXP preserve_group_dimension) {
+  BEGIN_CPP11
+    return cpp11::as_sexp(dust2_system_SEIRModelVtrack_state(cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(ptr), cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(r_index_state), cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(r_index_particle), cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(r_index_group), cpp11::as_cpp<cpp11::decay_t<bool>>(preserve_particle_dimension), cpp11::as_cpp<cpp11::decay_t<bool>>(preserve_group_dimension)));
+  END_CPP11
+}
+// SEIRModelVtrack.cpp
+SEXP dust2_system_SEIRModelVtrack_time(cpp11::sexp ptr);
+extern "C" SEXP _YellowFeverDynamics_dust2_system_SEIRModelVtrack_time(SEXP ptr) {
+  BEGIN_CPP11
+    return cpp11::as_sexp(dust2_system_SEIRModelVtrack_time(cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(ptr)));
+  END_CPP11
+}
+// SEIRModelVtrack.cpp
+SEXP dust2_system_SEIRModelVtrack_set_state_initial(cpp11::sexp ptr);
+extern "C" SEXP _YellowFeverDynamics_dust2_system_SEIRModelVtrack_set_state_initial(SEXP ptr) {
+  BEGIN_CPP11
+    return cpp11::as_sexp(dust2_system_SEIRModelVtrack_set_state_initial(cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(ptr)));
+  END_CPP11
+}
+// SEIRModelVtrack.cpp
+SEXP dust2_system_SEIRModelVtrack_set_state(cpp11::sexp ptr, cpp11::list r_state);
+extern "C" SEXP _YellowFeverDynamics_dust2_system_SEIRModelVtrack_set_state(SEXP ptr, SEXP r_state) {
+  BEGIN_CPP11
+    return cpp11::as_sexp(dust2_system_SEIRModelVtrack_set_state(cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(ptr), cpp11::as_cpp<cpp11::decay_t<cpp11::list>>(r_state)));
+  END_CPP11
+}
+// SEIRModelVtrack.cpp
+SEXP dust2_system_SEIRModelVtrack_reorder(cpp11::sexp ptr, cpp11::integers r_index);
+extern "C" SEXP _YellowFeverDynamics_dust2_system_SEIRModelVtrack_reorder(SEXP ptr, SEXP r_index) {
+  BEGIN_CPP11
+    return cpp11::as_sexp(dust2_system_SEIRModelVtrack_reorder(cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(ptr), cpp11::as_cpp<cpp11::decay_t<cpp11::integers>>(r_index)));
+  END_CPP11
+}
+// SEIRModelVtrack.cpp
+SEXP dust2_system_SEIRModelVtrack_rng_state(cpp11::sexp ptr);
+extern "C" SEXP _YellowFeverDynamics_dust2_system_SEIRModelVtrack_rng_state(SEXP ptr) {
+  BEGIN_CPP11
+    return cpp11::as_sexp(dust2_system_SEIRModelVtrack_rng_state(cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(ptr)));
+  END_CPP11
+}
+// SEIRModelVtrack.cpp
+SEXP dust2_system_SEIRModelVtrack_set_rng_state(cpp11::sexp ptr, cpp11::sexp r_rng_state);
+extern "C" SEXP _YellowFeverDynamics_dust2_system_SEIRModelVtrack_set_rng_state(SEXP ptr, SEXP r_rng_state) {
+  BEGIN_CPP11
+    return cpp11::as_sexp(dust2_system_SEIRModelVtrack_set_rng_state(cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(ptr), cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(r_rng_state)));
+  END_CPP11
+}
+// SEIRModelVtrack.cpp
+SEXP dust2_system_SEIRModelVtrack_set_time(cpp11::sexp ptr, cpp11::sexp r_time);
+extern "C" SEXP _YellowFeverDynamics_dust2_system_SEIRModelVtrack_set_time(SEXP ptr, SEXP r_time) {
+  BEGIN_CPP11
+    return cpp11::as_sexp(dust2_system_SEIRModelVtrack_set_time(cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(ptr), cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(r_time)));
+  END_CPP11
+}
+// SEIRModelVtrack.cpp
+SEXP dust2_system_SEIRModelVtrack_update_pars(cpp11::sexp ptr, cpp11::list pars);
+extern "C" SEXP _YellowFeverDynamics_dust2_system_SEIRModelVtrack_update_pars(SEXP ptr, SEXP pars) {
+  BEGIN_CPP11
+    return cpp11::as_sexp(dust2_system_SEIRModelVtrack_update_pars(cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(ptr), cpp11::as_cpp<cpp11::decay_t<cpp11::list>>(pars)));
+  END_CPP11
+}
+// SEIRModelVtrack.cpp
+SEXP dust2_system_SEIRModelVtrack_simulate(cpp11::sexp ptr, cpp11::sexp r_times, cpp11::sexp r_index_state, bool preserve_particle_dimension, bool preserve_group_dimension);
+extern "C" SEXP _YellowFeverDynamics_dust2_system_SEIRModelVtrack_simulate(SEXP ptr, SEXP r_times, SEXP r_index_state, SEXP preserve_particle_dimension, SEXP preserve_group_dimension) {
+  BEGIN_CPP11
+    return cpp11::as_sexp(dust2_system_SEIRModelVtrack_simulate(cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(ptr), cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(r_times), cpp11::as_cpp<cpp11::decay_t<cpp11::sexp>>(r_index_state), cpp11::as_cpp<cpp11::decay_t<bool>>(preserve_particle_dimension), cpp11::as_cpp<cpp11::decay_t<bool>>(preserve_group_dimension)));
+  END_CPP11
+}
 // SEIRVModelDelay.cpp
 SEXP dust2_system_SEIRVModelDelay_alloc(cpp11::list r_pars, cpp11::sexp r_time, cpp11::list r_time_control, cpp11::sexp r_n_particles, cpp11::sexp r_n_groups, cpp11::sexp r_seed, cpp11::sexp r_deterministic, cpp11::sexp r_n_threads);
 extern "C" SEXP _YellowFeverDynamics_dust2_system_SEIRVModelDelay_alloc(SEXP r_pars, SEXP r_time, SEXP r_time_control, SEXP r_n_particles, SEXP r_n_groups, SEXP r_seed, SEXP r_deterministic, SEXP r_n_threads) {
@@ -260,6 +344,18 @@ extern "C" SEXP _YellowFeverDynamics_dust2_system_SEIRVModelSplitInfection_simul
 
 extern "C" {
 static const R_CallMethodDef CallEntries[] = {
+    {"_YellowFeverDynamics_dust2_system_SEIRModelVtrack_alloc",                      (DL_FUNC) &_YellowFeverDynamics_dust2_system_SEIRModelVtrack_alloc,                      8},
+    {"_YellowFeverDynamics_dust2_system_SEIRModelVtrack_reorder",                    (DL_FUNC) &_YellowFeverDynamics_dust2_system_SEIRModelVtrack_reorder,                    2},
+    {"_YellowFeverDynamics_dust2_system_SEIRModelVtrack_rng_state",                  (DL_FUNC) &_YellowFeverDynamics_dust2_system_SEIRModelVtrack_rng_state,                  1},
+    {"_YellowFeverDynamics_dust2_system_SEIRModelVtrack_run_to_time",                (DL_FUNC) &_YellowFeverDynamics_dust2_system_SEIRModelVtrack_run_to_time,                2},
+    {"_YellowFeverDynamics_dust2_system_SEIRModelVtrack_set_rng_state",              (DL_FUNC) &_YellowFeverDynamics_dust2_system_SEIRModelVtrack_set_rng_state,              2},
+    {"_YellowFeverDynamics_dust2_system_SEIRModelVtrack_set_state",                  (DL_FUNC) &_YellowFeverDynamics_dust2_system_SEIRModelVtrack_set_state,                  2},
+    {"_YellowFeverDynamics_dust2_system_SEIRModelVtrack_set_state_initial",          (DL_FUNC) &_YellowFeverDynamics_dust2_system_SEIRModelVtrack_set_state_initial,          1},
+    {"_YellowFeverDynamics_dust2_system_SEIRModelVtrack_set_time",                   (DL_FUNC) &_YellowFeverDynamics_dust2_system_SEIRModelVtrack_set_time,                   2},
+    {"_YellowFeverDynamics_dust2_system_SEIRModelVtrack_simulate",                   (DL_FUNC) &_YellowFeverDynamics_dust2_system_SEIRModelVtrack_simulate,                   5},
+    {"_YellowFeverDynamics_dust2_system_SEIRModelVtrack_state",                      (DL_FUNC) &_YellowFeverDynamics_dust2_system_SEIRModelVtrack_state,                      6},
+    {"_YellowFeverDynamics_dust2_system_SEIRModelVtrack_time",                       (DL_FUNC) &_YellowFeverDynamics_dust2_system_SEIRModelVtrack_time,                       1},
+    {"_YellowFeverDynamics_dust2_system_SEIRModelVtrack_update_pars",                (DL_FUNC) &_YellowFeverDynamics_dust2_system_SEIRModelVtrack_update_pars,                2},
     {"_YellowFeverDynamics_dust2_system_SEIRVModelDelayReactive_alloc",              (DL_FUNC) &_YellowFeverDynamics_dust2_system_SEIRVModelDelayReactive_alloc,              8},
     {"_YellowFeverDynamics_dust2_system_SEIRVModelDelayReactive_reorder",            (DL_FUNC) &_YellowFeverDynamics_dust2_system_SEIRVModelDelayReactive_reorder,            2},
     {"_YellowFeverDynamics_dust2_system_SEIRVModelDelayReactive_rng_state",          (DL_FUNC) &_YellowFeverDynamics_dust2_system_SEIRVModelDelayReactive_rng_state,          1},
